5203f5abe6e096387d671672a91950e0821aa842
rockfield <me@philipp.ninja>
2015-07-31 00:18:24 +0300

    front components, settings, sprites, front template engine, sass modules and so on

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,35 @@ module.exports = function(grunt) {
 					ext: '.css'
 				}]
 			}
+		},
+		watch: {
+			sass: {
+				files: ['skin/**/*.scss'],
+				tasks: ['sass'],
+				options: {
+					spawn: true
+				}
+			}
+		},
+		sprite: {
+			all: {
+				src: ['skin/img/icons/_*.png'],
+				dest: 'skin/img/icon-pack.png',
+				destCss: 'skin/css/icon-pack.css',
+				algorithm: 'binary-tree',
+				padding: 3,
+				cssVarMap: function (sprite) {
+					sprite.name = sprite.name.replace(/^_/,'');
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.registerTask('default', ['sass']);
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-spritesmith');
+
+	grunt.registerTask('default', function(){
+		console.log('Please, use specific task: don\'t be lazy :)');
+	});
 };
